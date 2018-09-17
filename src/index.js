a491748282a911d31de190d310261f473a34790b
Mick de Graaf <mick@mickdegraaf.nl>
2018-09-17 15:59:56 +0200

    cast index to string

--- src/index.js
+++ src/index.js
@@ -6,16 +6,17 @@ const app = express();
 const port = 3000;
 
 
-var index = fs.readFileSync('src/index.html');
+var index = fs.readFileSync('src/index.html', 'utf8');
 
 // respond with "hello world" when a GET request is made to the homepage
 app.all('/', function (req, res) {
 
   piWifi.scan(function(err, networks) {
     if (err) {
-        return console.error(err.message);
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(":(");
+        console.log(console.log(JSON.stringify(index)));
+        return console.error(err.message);
     }
 
     let networkOptions = "";
